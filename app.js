1f106886b811b04e642535523a186ec9e3a82f81
Anawin Wongsadit <imonmac@hotmail.com>
2018-07-21 10:13:29 +0700

    Update Shower API

--- app.js
+++ app.js
@@ -14,32 +14,46 @@ let checkLoadCnt = function (total, callback) {
 let shower = {
     dataList: [''],
 
-    updateData: function (name) {
+    userId: -1,
+    isFirstLoaded: false,
+
+
+    updateData: function (key) {
+        exceed.getVal(key, function (resp) {
+            isLoaded[key] = 1
+            checkLoadCnt(shower.dataList.length, function () {
+                $('#cover-spin').hide();
+            })
+
+            console.log(`<updateData> ${key} RESP: ${resp}`)
+            if (resp == null || resp == "") {
+                this[key] = "N/A"
+            } else {
+                this[key] = resp
+            }
+
+            //TODO: HTML Change
+
+        })
+    },
+    updateData: function () {
+        if(!this.isFirstLoaded){
+            this.isFirstLoaded = true
+            exceed.getVal("lastUserId",function(resp){
+                shower.userId = parseInt(resp)+1
+            })
+        }
         for (var i = 0; i < this.dataList.length; i++) {
             console.log(`<updateData> ${this.dataList[i]} START!`)
             let key = this.dataList[i]
-            exceed.getVal(key, function (resp) {
-                isLoaded[key] = 1
-                checkLoadCnt(shower.dataList.length, function () {
-                    $('#cover-spin').hide();
-                })
-
-                console.log(`<updateData> ${key} RESP: ${resp}`)
-                if (resp == null || resp == "") {
-                    this[key] = "N/A"
-                } else {
-                    this[key] = resp
-                }
-
-                //TODO: HTML Change
-
-            })
+            this.updateData(key)
         }
-    }
+    },
+
 }
 $(function () {
-    exceed.configure({url:'http://ecourse.cpe.ku.ac.th:1515/api/',prefix:'palmyut-'})
-    //$('#cover-spin').show(0)
+    exceed.configure({url:'http://ecourse.cpe.ku.ac.th/exceed/api/',prefix:'palmyut-'})
+    //0$('#cover-spin').show(0)
 
     shower.updateData()
     setInterval(function () { shower.updateData() }, 7000)
